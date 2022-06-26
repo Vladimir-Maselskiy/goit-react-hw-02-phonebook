fd01a28e4ea6019859498a2c564a1f89924677ea
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-06-26 13:54:54 +0300

    add yup

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,42 +1,52 @@
 import { nanoid } from 'nanoid';
-import { Box, InputForm } from './Form.styled';
+import { Box, ErrorStiled, InputForm } from './Form.styled';
 import { Formik, Field } from 'formik';
+import * as yup from 'yup';
+
+const nameInputId = nanoid();
+const numberInputId = nanoid();
+const initialValues = { name: 'Billy', number: '1234567' };
+
+let schema = yup.object().shape({
+  name: yup
+    .string()
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    )
+    .required(),
+  number: yup
+    .number()
+    .matches(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    )
+    .required(),
+});
 
 export function Form(props) {
-  const nameInputId = nanoid();
-  const numberInputId = nanoid();
-  const initialValues = { name: 'Billy', number: '1234567' };
-
   function handleFormSubmit(values, actions) {
-    props.onSubmit(values);
+    if (!props.onSubmit(values)) return;
     actions.resetForm();
   }
 
   return (
     <Box>
-      <Formik initialValues={initialValues} onSubmit={handleFormSubmit}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={schema}
+        onSubmit={handleFormSubmit}
+      >
         <InputForm>
           <label htmlFor={nameInputId}>
             Name
-            <Field
-              id={nameInputId}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
+            <Field id={nameInputId} type="text" name="name" />
+            <ErrorStiled name="name" component="div" />
           </label>
           <label htmlFor={numberInputId}>
             Number
-            <Field
-              id={numberInputId}
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
+            <Field id={numberInputId} type="tel" name="number" />
+            <ErrorStiled name="number" component="div" />
           </label>
           <button type="submit">Add Contact</button>
         </InputForm>
--- src/components/Form/Form.styled.jsx
+++ src/components/Form/Form.styled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Form } from 'formik';
+import { Form, ErrorMessage } from 'formik';
 
 export const Box = styled.div`
   display: flex;
@@ -23,8 +23,10 @@ export const InputForm = styled(Form)`
   height: 100%;
   margin: 0 auto;
   label {
+    position: relative;
     width: 100%;
     display: flex;
+    flex-wrap: wrap;
     justify-content: space-between;
   }
   input {
@@ -33,4 +35,19 @@ export const InputForm = styled(Form)`
   button {
     margin-left: auto;
   }
+`;
+
+export const ErrorStiled = styled(ErrorMessage)`
+  color: red;
+  font-size: 12px;
+  position: absolute;
+  top: 20px;
+  right: 100px;
+  background-color: #ffffff;
+  padding: 5px;
+  width: 100%;
+  z-index: 2;
+  text-align: justify;
+  border: solid 1px #ccc;
+  border-radius: 10px;
 `;
